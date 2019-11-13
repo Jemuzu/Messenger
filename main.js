a72fbe3de96d0f8ab7b611f833db8056b0f68906
James Crawford <jcrawford@nyc.yearup.org>
2019-11-13 15:56:45 -0500

    Added a new js file and updated original js file

--- main.js
+++ main.js
@@ -1,39 +1,84 @@
-// A user needs to be able to send messages.
+/* A user needs to be able to send messages.
 // Messages need to be stored with the user they belong to.
 // Needs to be able to read individual messagees
 
 // Classes start with a capital letter
 // 95% of the time, class are singual in name
 // user.inbox => that person's Messages
-//user.sendMessage('Ryan' , 'Heybb')
+user.sendMessage('Ryan' , 'Heybb */
+
+const Conversations = []
+
+class Conversation {
+  constructor() {
+    this.id = Conversations.length + 1
+    this.messages = []
+    this.users = []
+    Conversations.push(this)
+  }
+
+add(user){
+  this.users.push(user)
+  }
+}
 
 class User {
   constructor(name) {
     this.name = name
     this.inbox = []
-   }
+    this.blockedUsers = []
+  }
 
-sendMessage(receiver, content) {
-  let msg = new Message(receiver, content)
-  receiver.inbox.push(msg)
-  return `Your message to ${receiver.name}, has been sent`
+sendMessage(convo, content) {
+  //if (convo.blockedUsers.includes(this)) return 'Omae wa mou'
+  let msg = new Message(convo, content)
+  convo.inbox.push(msg)
 }
 
 readMessage(i) {
-  return this.inbox[i].content
+this.inbox[i].seen = true
+this.inbox[i].seenAt = new Date()
+return this.inbox[i].content
+/*var d = new Date();
+d.setTime(mm/dd/yy);*/
+}
+
+viewUnread() {
+  let unread = []
+  for (msg of this.inbox) { //block scoping// zzzzzzzzzz
+    if (msg.seen === false) {
+      unread.push(msg)
+      console.log(msg.content)
+      }
+    }
+      return unread
   }
 }
 
 class Message {
-  constructor(reciever, content) { // Gives us the keyword new
-    this.receiver = receiver
+  constructor(content) { // Gives us the keyword new
+    this.receiver = convo
     this.content = content
+    this.seen = false
+    this.seenAt = []
+  }
+  viewRead() {
+    let read = []
+    for (let msg of this.inbox) { //block scoping// zzzzzzzzzz
+      if (msg.seen === true) {
+        read.push(msg)
+        console.log(msg.content)
+      }
+}
+    return read
+  }
+  block(user) {
+    this.blockedUsers.push(User)
   }
 }
 
-
-
-
+let user1 = new User('James')
+let user2 = new User('Joker')
 // any object based on a class is called an instance
 // An instance represents one object based on the class name
 // Constructor arguments go in the pa
